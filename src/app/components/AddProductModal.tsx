f70833b34bda527b2644b20dfa39981541bcf388
Aman <amansinghofficial2004@gmail.com>
2026-04-10 11:10:01 +0530

    removed bug from add product modal

--- src/app/components/AddProductModal.tsx
+++ src/app/components/AddProductModal.tsx
@@ -20,7 +20,7 @@ export default function AddProductModal() {
             <form id="add-product-form" className="space-y-5">
 
                 <div>
-                    <label className="text-sm font-medium text-gray-700">
+                    <label className="text-sm font-medium">
                         Title
                     </label>
                     <p className="text-xs text-gray-400 mb-1">Brand Vehicle product name</p>
@@ -36,37 +36,37 @@ export default function AddProductModal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
                     <div>
-                        <label className="text-sm font-medium text-gray-700">
+                        <label className="text-sm font-medium">
                             Category<span className="text-red-500">*</span>
                         </label>
-                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-600">
+                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm ">
                             <option>Choose category</option>
                         </select>
                     </div>
 
                     <div>
-                        <label className="text-sm font-medium text-gray-700">
+                        <label className="text-sm font-medium ">
                             Company<span className="text-red-500">*</span>
                         </label>
-                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-600">
+                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm ">
                             <option>Choose company</option>
                         </select>
                     </div>
 
                     <div>
-                        <label className="text-sm font-medium text-gray-700">
+                        <label className="text-sm font-medium ">
                             Division<span className="text-red-500">*</span>
                         </label>
-                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-600">
+                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm ">
                             <option>Choose division</option>
                         </select>
                     </div>
 
                     <div>
-                        <label className="text-sm font-medium text-gray-700">
+                        <label className="text-sm font-medium ">
                             Segment<span className="text-red-500">*</span>
                         </label>
-                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-600">
+                        <select className="w-full mt-1 border border-gray-300 rounded-md px-3 py-2 text-sm ">
                             <option>Choose segment</option>
                         </select>
                     </div>
@@ -74,12 +74,12 @@ export default function AddProductModal() {
                 </div>
 
                 <div>
-                    <label className="text-sm font-medium text-gray-700">
+                    <label className="text-sm font-medium ">
                         Description<span className="text-red-500">*</span>
                     </label>
 
                     <div
-                        className="border border-gray-300 rounded-t-md px-3 py-2 flex gap-3 text-sm text-gray-600 bg-gray-50">
+                        className="border border-gray-300 rounded-t-md px-3 py-2 flex gap-3 text-sm  bg-gray-50">
                         <button type="button" className="font-bold">B</button>
                         <button type="button" className="italic">I</button>
                         <button type="button" className="underline">U</button>
@@ -90,7 +90,7 @@ export default function AddProductModal() {
                     </div>
 
                     <textarea
-                        className="w-full border border-t-0 border-gray-300 rounded-b-md px-3 py-2 h-40 focus:outline-none focus:ring-2 focus:ring-red-500"></textarea>
+                        className="w-full border border-t-0 border-gray-300 rounded-b-md px-3 py-2 h-40 focus:outline-none focus:ring-2 focus:ring-red-500 text-black"></textarea>
                 </div>
 
             </form>
--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,45 +1,62 @@
 "use client"
 import { useView } from "@/app/context/view-context";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
 
 export default function ProductCard({ title, category, type, image }: any) {
     const { view } = useView();
-    
-    return view === "grid" ?
-        (
-            <div
-                className="group relative rounded-md flex flex-col overflow-hidden text-white w-full h-45 sm:h-70 md:h-85 lg:h-45 xl:h-55 2xl:h-60 product-item">
-                <img src={image} className="w-full h-full object-cover" alt="" />
-                <div className="absolute inset-0 bg-black/40 group-hover:bg-black/0 transition-300"></div>
-                <div className="absolute flex flex-col justify-center bottom-3 left-1 p-2 text-xs">
-                    <h2>{title}</h2>
-                    <p>{category}</p>
-                </div>
-            </div>
-        )
-        :
-        (
-            <div
-                className="grid grid-cols-8 gap-4 items-center bg-white rounded-lg px-4 py-3 shadow-sm hover:bg-gray-50 transition">
 
-                <div className="flex items-center col-span-3 gap-2">
-                    <img src="assets/images/card.jpg" className="w-10 h-10 rounded object-cover" />
-                    <span className="text-xs font-medium text-gray-800">
-                        {title}
-                    </span>
-                </div>
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.1,
+    });
 
-                <div className="text-xs text-gray-500 col-span-2">
-                    {category}
-                </div>
+    return (
+        <div ref={ref}>
+            {inView && (
+                view === "grid" ? (
+                    <div className="group relative rounded-md flex flex-col overflow-hidden text-white w-full h-45 sm:h-70 md:h-85 lg:h-45 xl:h-55 2xl:h-60 product-item">
+                        
+                        <Image
+                            src={image}
+                            alt={title}
+                            fill
+                            className="rounded object-cover"
+                        />
 
-                <div className="text-sm text-gray-500 col-span-2">
-                    {type}
-                </div>
+                        <div className="absolute inset-0 bg-black/40 group-hover:bg-black/0 transition-300"></div>
 
-                <div className="flex justify-end text-gray-400 col-span-1">
-                    &#8594;
-                </div>
+                        <div className="absolute flex flex-col justify-center bottom-3 left-1 p-2 text-xs">
+                            <h2>{title}</h2>
+                            <p>{category}</p>
+                        </div>
 
-            </div>
-        )
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-8 gap-4 items-center bg-white rounded-lg px-4 py-3 shadow-sm hover:bg-gray-50 transition">
+                        
+                        <div className="flex items-center col-span-3 gap-2">
+                            <img src={image} className="w-10 h-10 rounded object-cover" />
+                            <span className="text-xs font-medium text-gray-800">
+                                {title}
+                            </span>
+                        </div>
+
+                        <div className="text-xs text-gray-500 col-span-2">
+                            {category}
+                        </div>
+
+                        <div className="text-sm text-gray-500 col-span-2">
+                            {type}
+                        </div>
+
+                        <div className="flex justify-end text-gray-400 col-span-1">
+                            &#8594;
+                        </div>
+
+                    </div>
+                )
+            )}
+        </div>
+    );
 }
