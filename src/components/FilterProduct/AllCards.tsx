1239482ac53e34f39866727a473a2c5a21bd99c8
aman <amansinghofficial2004@gmail.com>
2026-04-21 23:49:27 +0530

    fixed all images url

--- src/components/FilterProduct/AllCards.tsx
+++ src/components/FilterProduct/AllCards.tsx
@@ -6,11 +6,11 @@ interface Props {
     total_products: number
 }
 export default function AllCards({ name, description="abc", image, total_products }: Props) {
-
+    const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
     return (
         <div className="flex bg-white h-30 w-full mb-2 p-2 overflow-hidden cursor-pointer">
             <div className="basis-1/3 p-2">
-                <img src={`http://127.0.0.1:8000/storage/images/divisions/${image}`} alt="" className="rounded-md w-full h-full object-cover" />
+                <img src={`${baseURL}/storage/images/divisions/${image}`} alt="" className="rounded-md w-full h-full object-cover" />
             </div>
             <div className="basis-2/3">
                 <h3 className="font-bold text-sm">
